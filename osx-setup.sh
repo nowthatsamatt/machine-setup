4f0ff3bba5c67501ebf84aed0b6141772fd5508e
Matthew Cupples <matt.cupples@tubemogul.com>
2018-08-17 10:02:26 -0700

    updating symlinks for machine recovery
    Change-Id: I504c61efecaa7a59803bc0120b05026d1d23860c

--- osx-setup.sh
+++ osx-setup.sh
@@ -52,17 +52,20 @@ ln -s /Users/${username}/Dropbox/tubemogul/configs/ApacheDirectoryStudio/ ~/.Apa
 ln -s /Users/${username}/Dropbox/tubemogul/configs/aws ~/.aws
 ln -s /Users/${username}/Dropbox/tubemogul/configs/bash_eternal_history ~/.bash_eternal_history
 ln -s /Users/${username}/Dropbox/tubemogul/configs/bash_profile ~/.bash_profile
-ln -s /Users/${username}/Dropbox/cloud_credentials ~/.cloud_credentials
+ln -s /Users/${username}/Dropbox/tubemogul/configs/cloud_credentials ~/.cloud_credentials
 ln -s /Users/${username}/Dropbox/tubemogul/configs/eyaml ~/.eyaml
-ln -s /Users/${username}/Dropbox/tubemogul/configs/gist ~/.gist
+ln -s /Users/${username}/Dropbox/tubemogul/configs/gist.https.git.corp.adobe.com .gist.https.git.corp.adobe.com
 ln -s /Users/${username}/Dropbox/tubemogul/configs/git-templates ~/.git-templates
 ln -s /Users/${username}/Dropbox/tubemogul/configs/gitconfig ~/.gitconfig
 ln -s /Users/${username}/Dropbox/tubemogul/configs/gnupg ~/.gnupg
 ln -s /Users/${username}/Dropbox/tubemogul/configs/ipython/ ~/.ipython
 ln -s /Users/${username}/Dropbox/tubemogul/configs/irssi/ ~/.irssi
+ln -s /Users/${username}/Dropbox/tubemogul/configs/kube ~/.kube
 ln -s /Users/${username}/Dropbox/tubemogul/configs/packer.d/ ~/.packer.d
 ln -s /Users/${username}/Dropbox/tubemogul/configs/parallel/ ~/.parallel
+ln -s /Users/${username}/Dropbox/tubemogul/configs/pydistutils.cfg ~/.pydistutils.cfg
 ln -s /Users/${username}/Dropbox/tubemogul/configs/ssh/ ~/.ssh
+ln -s /Users/${username}/Dropbox/tubemogul/configs/sshuttle.conf ~/.sshuttle.conf
 ln -s /Users/${username}/Dropbox/tubemogul/configs/terraform.d/ ~/.terraform.d
 ln -s /Users/${username}/Dropbox/tubemogul/configs/tmux.conf ~/.tmux.conf
 ln -s /Users/${username}/Dropbox/tubemogul/configs/vault-token ~/.vault-token
@@ -71,6 +74,10 @@ ln -s /Users/${username}/Dropbox/tubemogul/configs/vimrc ~/.vimrc
 ln -s /Users/${username}/Dropbox/tubemogul/configs/vscode/ ~/.vscode
 ln -s /Users/${username}/Dropbox/tubemogul/configs/vsql_history ~/.vsql_history
 ln -s /Users/${username}/Dropbox/tm-repos/ ~/tm
+ln -s /Users/${username}/Dropbox/search-repos/ ~/search
+
+# resolver setup
+sudo ln -s /Users/${username}/Dropbox/tubemogul/configs/resolver /etc/resolver
 
 # install other software
 #vscode
